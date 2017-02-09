9569482aa3777d6c36bca754d961d5d30b04418f
vadimivanov <Vjcrdf0508>
2017-02-09 16:15:55 +0200

    add clinical-statements plugin

---
+++ clinical-statements/ActionTypes.js
@@ -0,0 +1,34 @@
+/*
+ ~  Copyright 2016 Ripple Foundation C.I.C. Ltd
+ ~  
+ ~  Licensed under the Apache License, Version 2.0 (the "License");
+ ~  you may not use this file except in compliance with the License.
+ ~  You may obtain a copy of the License at
+ ~  
+ ~    http://www.apache.org/licenses/LICENSE-2.0
+
+ ~  Unless required by applicable law or agreed to in writing, software
+ ~  distributed under the License is distributed on an "AS IS" BASIS,
+ ~  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~  See the License for the specific language governing permissions and
+ ~  limitations under the License.
+ */
+export const CLINICALSTATEMENTS = 'CLINICALSTATEMENTS';
+export const CLINICALSTATEMENTS_SUCCESS = 'CLINICALSTATEMENTS_SUCCESS';
+export const CLINICALSTATEMENTS_ERROR = 'CLINICALSTATEMENTS_ERROR';
+
+export const CLINICALSTATEMENTS_GET = 'CLINICALSTATEMENTS_GET';
+export const CLINICALSTATEMENTS_GET_SUCCESS = 'CLINICALSTATEMENTS_GET_SUCCESS';
+export const CLINICALSTATEMENTS_GET_ERROR = 'CLINICALSTATEMENTS_GET_ERROR';
+
+export const CLINICALSTATEMENTS_CREATE = 'CLINICALSTATEMENTS_CREATE';
+export const CLINICALSTATEMENTS_CREATE_SUCCESS = 'CLINICALSTATEMENTS_CREATE_SUCCESS';
+export const CLINICALSTATEMENTS_CREATE_ERROR = 'CLINICALSTATEMENTS_CREATE_ERROR';
+
+export const CLINICALSTATEMENTS_UPDATE = 'CLINICALSTATEMENTS_UPDATE';
+export const CLINICALSTATEMENTS_UPDATE_SUCCESS = 'CLINICALSTATEMENTS_UPDATE_SUCCESS';
+export const CLINICALSTATEMENTS_UPDATE_ERROR = 'CLINICALSTATEMENTS_UPDATE_ERROR';
+
+export const CLINICALSTATEMENTS_QUERY = 'CLINICALSTATEMENTS_QUERY';
+export const CLINICALSTATEMENTS_QUERY_SUCCESS = 'CLINICALSTATEMENTS_QUERY_SUCCESS';
+export const CLINICALSTATEMENTS_QUERY_ERROR = 'CLINICALSTATEMENTS_QUERY_ERROR';
---
+++ clinical-statements/index.route.js
@@ -0,0 +1,42 @@
+/*
+ ~  Copyright 2016 Ripple Foundation C.I.C. Ltd
+ ~  
+ ~  Licensed under the Apache License, Version 2.0 (the "License");
+ ~  you may not use this file except in compliance with the License.
+ ~  You may obtain a copy of the License at
+ ~  
+ ~    http://www.apache.org/licenses/LICENSE-2.0
+
+ ~  Unless required by applicable law or agreed to in writing, software
+ ~  distributed under the License is distributed on an "AS IS" BASIS,
+ ~  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ ~  See the License for the specific language governing permissions and
+ ~  limitations under the License.
+ */
+routeConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+
+function routeConfig($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/search');
+  console.log('routing test');
+  $stateProvider
+
+      .state('clinicalStatements', {
+        url: '/patients/{patientId:int}/clinicalStatements?reportType&searchString&queryType',
+        views: {
+          banner: {template: '<patients-banner-component></patients-banner-component>'},
+          actions: {template: '<patients-sidebar-component></patients-sidebar-component>'},
+          main: {template: '<clinicalStatements-list-component></clinicalStatements-list-component>'}
+        }
+      })
+      .state('clinicalStatements-detail', {
+        url: '/patients/{patientId:int}/clinicalStatements/{clinicalStatementIndex}?filter&page&reportType&searchString&queryType&source',
+        views: {
+          banner: {template: '<patients-banner-component></patients-banner-component>'},
+          actions: {template: '<patients-sidebar-component></patients-sidebar-component>'},
+          main: {template: '<clinicalStatements-list-component></clinicalStatements-list-component>'},
+          detail: {template: '<clinicalStatements-detail-component></clinicalStatements-detail-component>'}
+        }
+      });
+}
+
+export default routeConfig;
